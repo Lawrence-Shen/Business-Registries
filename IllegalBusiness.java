fb1d30423e002c440122ee0cadd3195c15a6e23b
Lawrence Shen <145161879+Lawrence-Shen@users.noreply.github.com>
2023-09-17 18:57:49 -0400

    Create IllegalBusiness.java

---
+++ IllegalBusiness.java
@@ -0,0 +1,8 @@
+package businessRegistry;
+
+//General exception for any unwanted inputs
+public class IllegalBusiness extends Exception{
+	public IllegalBusiness(String message) {
+		super(message);
+	}
+}
